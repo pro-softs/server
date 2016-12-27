e9be685fe9dbb459333afd6d1e3bbee395ba1587
Pro gupta <pchemistry.35@gmail.com>
2016-12-27 16:18:05 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,16 @@
-var Location = require('../models/Jams.js');
+var Location = require('../models/Location.js');
 
 module.exports = function(app) {
 
 	app.get('/', function(req, res) {
 		res.json({ message: 'welcome to our api!' });
+	});
+	
+	app.post('/location', function(req, res) {
+		var loc = req.location;
+		var updated_at = req.updated_at;
+		
+		
 	});
 
 	
